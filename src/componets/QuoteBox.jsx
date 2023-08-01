abd03c4265d3da843dab49b68568cd7898df2fdf
Fabianrealp <realpe2003@gmail.com>
2023-08-01 13:03:37 -0500

    ultima modificaciont

--- src/componets/QuoteBox.jsx
+++ src/componets/QuoteBox.jsx
@@ -1,8 +1,13 @@
 import "./styles/QuoteBox.css";
 import { getRandon } from "../utils/ramdom";
 
-const imgPlanets=["/images/planet1.1.jpg","/images/planet2.2.jpg","/images/planet3.3.jpg","/images/planet4.4.jpg"]
-const QuoteBox = ({ handleChangeQuote, quote}) => {
+const imgPlanets = [
+  "/images/planet1.1.jpg",
+  "/images/planet2.2.jpg",
+  "/images/planet3.3.jpg",
+  "/images/planet4.4.jpg",
+];
+const QuoteBox = ({ handleChangeQuote, quote }) => {
   return (
     <section className="quoteBox">
       <h1>INFOGALAX</h1>
@@ -14,14 +19,13 @@ const QuoteBox = ({ handleChangeQuote, quote}) => {
           <box-icon name="refresh" className="refresh"></box-icon>
         </button>
       </section>
-      
+
       <footer className="imgposition">
-      <img src= {getRandon(imgPlanets)}/>
+        <img src={getRandon(imgPlanets)} />
         <h4>Fuente: {quote.author}</h4>
       </footer>
-      
     </section>
   );
 };
 
-export default QuoteBox
+export default QuoteBox;
